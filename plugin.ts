bcc2f037517527501a8332612dc93dd2e84072cc
Mark Dierolf <mdierolf@cloudquote.io>
2026-04-06 14:50:21 -0700

    Update plugin metadata, schemas, and refine descriptions

--- plugin.ts
+++ plugin.ts
@@ -10,6 +10,7 @@ const packageConfigSchema = z.object({
 
 export default {
   name: packageJSON.name,
+  displayName: "CDN Service",
   version: packageJSON.version,
   description: packageJSON.description,
   install(app, config) {
